c8fbcc7bf57091eec10e269b21a8c9f9d1e5dcd6
Kiel Arneson <kiel.arneson@gmail.com>
2021-09-30 10:04:38 -0500

    Added mathjs to test adding another npm

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var moment = require("moment");
+var math = require("mathjs");
 
 console.log("Hello from JS");
 
 var dateTime = moment().startOf("hour").fromNow();
 console.log(dateTime);
+
+console.log(math.sqrt(-4).toString());
