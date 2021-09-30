7ac9892e7180dfdf6a6b9b406f96f26c3d667396
Kiel Arneson <kiel.arneson@gmail.com>
2021-09-30 10:46:57 -0500

    Added Babel and created webpack config file to streamline webpack use in the terminal

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ var dateTime = moment().startOf("hour").fromNow();
 console.log(dateTime);
 
 console.log(math.sqrt(-4).toString());
+
+function g({ name: x }) {
+  console.log(x);
+}
+g({ name: 5 });
+
+function f(x, ...y) {
+  return x * y.length;
+}
+console.log(f(3, "hello", true, "bye"));
